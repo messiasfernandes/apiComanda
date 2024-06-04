df3b1d946ba604f50591687022e67ba0b80f201c
Messias Fernandes <66951793+messiasfernandes@users.noreply.github.com>
2024-06-03 23:08:14 -0300

    mesa com total por comanda

--- src/main/java/com/comanda/domain/entity/Mesa.java
+++ src/main/java/com/comanda/domain/entity/Mesa.java
@@ -2,6 +2,7 @@ package com.comanda.domain.entity;
 
 
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +14,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
@@ -34,7 +37,21 @@ public class Mesa extends GeradorId {
 	@Column(length = 30)
 	@Enumerated(EnumType.STRING)
 	private StatusMesa statusMesa;
-	
+	@Getter(value = AccessLevel.NONE)
+	@Transient
+	private BigDecimal total;
+	public BigDecimal getTotal() {
+		 calcularTotal();
+	        return total;
+	}
 	   @OneToMany(mappedBy = "mesa")
 	    private List<Comanda> comandas = new ArrayList<>();
+	   
+	   private void calcularTotal() {
+	        total = BigDecimal.ZERO;
+	        comandas.stream()
+            .flatMap(comanda -> comanda.getItemsdaComanda().stream())
+            .forEach(item -> total = total.add(item.getSubtotal()));
+	    }
+	  
 }
